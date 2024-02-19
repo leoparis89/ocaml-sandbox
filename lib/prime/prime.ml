50eff0555dab6a9486b5e40cc88f2c273fcd8f49
Lev Kowalski <lev.kowalski@ahrefs.com>
2024-02-19 12:46:15 +0100

    prime module

---
+++ lib/prime/prime.ml
@@ -0,0 +1,8 @@
+let is_prime n =
+  let rec is_prime_aux i =
+    if i > n then true
+    else if n mod i = 0 then false
+    else is_prime_aux (i + 1)
+  in
+  if n < 2 then false
+  else is_prime_aux 2
